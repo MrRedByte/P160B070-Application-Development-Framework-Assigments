88bf74e2c489a3f7cb65310cdfec32064695e629
P Claudiu-Alexandru <claudiuu.alex@gmail.com>
2026-03-18 17:56:49 +0200

    modified:   routes/web.php

--- StudentsKU2026/routes/web.php
+++ StudentsKU2026/routes/web.php
@@ -28,10 +28,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
